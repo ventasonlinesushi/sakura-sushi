5fb656bf686cf2ae0d293e7d7f404ecc0d0a24e2
loscaramirez-oss <loscaramirez@gmail.com>
2026-08-12 15:56:03 -0600

    Aclarar acciones de productos

--- admin/sw.js
+++ admin/sw.js
@@ -1,5 +1,5 @@
-const CACHE="sakura-pos-mobile-v11";
-const ASSETS=["./","index.html","admin.css?v=pos8","admin.js?v=pos8","manifest.json","icon-192.png","icon-512.png","../js/config/brand-config.js","../js/data/menu-data.js","../logo.png"];
+const CACHE="sakura-pos-mobile-v12";
+const ASSETS=["./","index.html","admin.css?v=pos8","admin.js?v=pos9","manifest.json","icon-192.png","icon-512.png","../js/config/brand-config.js","../js/data/menu-data.js","../logo.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET"||new URL(e.request.url).origin!==self.location.origin)return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./"))))});
