a385200295e1847f8880022fd1c8994c93ed4f6d
ventasonlinesushi <loscaramirez@gmail.com>
2026-09-23 19:25:13 -0600

    Mantener menú abierto tras agregar rollo con o sin alga

--- js/views/app-view.js
+++ js/views/app-view.js
@@ -76,7 +76,7 @@
         if (!selected) return;
         close();
         this.cart.changeQty(key + "|alga=" + selected.value, 1);
-        this.openDrawer();
+        // Mantener al cliente en el menú para que pueda seguir agregando platillos.
       };
     }
 
